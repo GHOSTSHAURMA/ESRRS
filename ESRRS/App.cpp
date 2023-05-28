e37ebac8c99210291352498242a6a0b9dbe53195
BuildTools <unconfigured@null.spigotmc.org>
2023-05-28 18:46:50 +0300

    added a simple(useless) tray notification

--- ESRRS/App.cpp
+++ ESRRS/App.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+const int batteryRR = 60, lineRR = 165;
+
 void changeDisplaySettings(DEVMODE dm, DWORD old = 0)
 {
     LONG ret = ChangeDisplaySettings(&dm, CDS_UPDATEREGISTRY | CDS_GLOBAL);
@@ -15,7 +17,7 @@ void changeDisplaySettings(DEVMODE dm, DWORD old = 0)
         sprintf_s(buff, "Refresh rate changed from %d to %d successfully", old, dm.dmDisplayFrequency);
         wchar_t wbuff[1025];
         mbstowcs_s(nullptr, wbuff, 1025, buff, 1024);
-        MessageBox(NULL, wbuff, L"VRR", NULL);
+        MessageBox(NULL, wbuff, L"ESRRS", NULL);
         break;
     }
     case DISP_CHANGE_BADPARAM:
@@ -30,7 +32,7 @@ void changeDisplaySettings(DEVMODE dm, DWORD old = 0)
         sprintf_s(buff, "To change refresh rate from %d to %d RESTART your PC", old, dm.dmDisplayFrequency);
         wchar_t wbuff[1025];
         mbstowcs_s(nullptr, wbuff, 1025, buff, 1024);
-        MessageBox(NULL, wbuff, L"VRR", NULL);
+        MessageBox(NULL, wbuff, L"ESRRS", NULL);
         break;
     }
     default:
@@ -50,17 +52,17 @@ void checkPoverAndSwitch()
     GetSystemPowerStatus(&powerStatus);
     if (powerStatus.ACLineStatus == 1)
     {
-        if (dm.dmDisplayFrequency == 165) return;
+        if (dm.dmDisplayFrequency == lineRR) return;
         DWORD old = dm.dmDisplayFrequency;
-        dm.dmDisplayFrequency = 165;
+        dm.dmDisplayFrequency = lineRR;
         dm.dmFields = DM_DISPLAYFREQUENCY;
         changeDisplaySettings(dm, old);
     }
     else
     {
-        if (dm.dmDisplayFrequency == 60) return;
+        if (dm.dmDisplayFrequency == batteryRR) return;
         DWORD old = dm.dmDisplayFrequency;
-        dm.dmDisplayFrequency = 60;
+        dm.dmDisplayFrequency = batteryRR;
         dm.dmFields = DM_DISPLAYFREQUENCY;
         changeDisplaySettings(dm, old);
     }
@@ -96,14 +98,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     WNDCLASS wc = { 0 };
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
-    wc.lpszClassName = L"ESVRR";
+    wc.lpszClassName = L"ESRRS";
     wc.hInstance = hInstance;
     wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
     wc.lpfnWndProc = WndProc;
 
 
     RegisterClass(&wc);
-    HWND hWnd = CreateWindowW(wc.lpszClassName, L"Energy-saving VRR", WS_OVERLAPPED | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 600, 300, NULL, NULL, hInstance, NULL);
+    HWND hWnd = CreateWindowW(wc.lpszClassName, L"ESRRS", WS_OVERLAPPED | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 600, 300, NULL, NULL, hInstance, NULL);
 
     if (!hWnd)
     {
@@ -115,6 +117,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 1;
     }
 
+    // No need for now
+    // Add settings later
+    // ShowWindow(hWnd, SHOW_OPENWINDOW);
+
+    NOTIFYICONDATAA nid = {};
+    nid.cbSize = sizeof(nid);
+    nid.hWnd = hWnd;
+    nid.uFlags = NIF_TIP;
+    memcpy(nid.szTip, "ESRRS\nEnergy-saving refresh rate switch", 128);
+
+    Shell_NotifyIconA(NIM_ADD, &nid);
+
     UpdateWindow(hWnd);
 
     while (GetMessage(&msg, NULL, 0, 0)) 
@@ -123,7 +137,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         DispatchMessage(&msg);
     }
 
-    RegisterPowerSettingNotification(hWnd, &GUID_ACDC_POWER_SOURCE, DEVICE_NOTIFY_WINDOW_HANDLE);
+    Shell_NotifyIconA(NIM_DELETE, &nid);
 
     return (int)msg.wParam;
 }
